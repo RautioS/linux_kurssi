ddd1a6d20d7d14aa22cfd8564e15e7d37d78b8a2
Santeri <santerirautio17@gmail.com>
2025-11-25 14:57:34 +0200

    Fixailuja

--- streamlit/chuck_norris.py
+++ streamlit/chuck_norris.py
@@ -7,12 +7,14 @@ st.title("Chuck Norris -vitsit")
 
 st.write("Hae satunnainen Chuck Norris -vitsi [Chuck Norris API](https://api.chucknorris.io/)")
 
-try:
-    response = requests.get("https://api.chucknorris.io/jokes/random")
-    if response.status_code == 200:
-        joke = response.json().get("value", "Ei vitsiä saatavilla.")
-        st.success(joke)
-    else:
-        st.error("Virhe API-kutsussa.")
-except Exception as e:
-    st.error(f"Tapahtui virhe: {e}")
+
+# Näytetään vitsi jokaisella napin painalluksella
+if st.button("Hae uusi vitsi"):
+    try:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+        if response.status_code == 200:
+            joke = response.json().get("value", "Ei vitsiä saatavilla.")
+            st.success(joke)
+        else:
+            st.error("Virhe API-kutsussa.")
+    except Exception as e:
